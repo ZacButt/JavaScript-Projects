fd28ce92f18346df5f543ba9f04a7e73c041ff8f
ZacButt <128622380+ZacButt@users.noreply.github.com>
2023-04-18 15:40:27 -0700

    Add files via upload
    Tried pushing through github desktop and it wouldnt work for some reason so hopefully this does!

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,14 @@
+document.write('Hello, World '); // printing hello world on the web page
+var A= "My Name is Zac!"
+document.write(A);
+var B= "I'm excited to learn about Javascript. "
+window.alert(B);
+document.write(" I told my friend, \"I want to have a good day, but I don't know what today is going to entail.\"");
+document.write("\" Be who you are and say what you feel," 
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Seuss");
+var C = " Concatenated" + " String "
+document.write(C)
+var Family = " The Butts ", Dad = "Rob ", Mom = "Meg ", Son = "Zac ", Daughter = "Sam "; // This is showing everyone in my family
+document.write(Son )  // When i specify "Son", it writes my name (Zac)
+document.write(2+4 );
