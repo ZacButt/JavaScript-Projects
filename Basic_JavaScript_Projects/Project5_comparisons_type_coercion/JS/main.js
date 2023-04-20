f31dce6019a8932cb34f6a0f6fbb474bbe1627ec
ZacButt <zacbutt8787@gmail.com>
2023-04-20 11:07:37 -0700

    Comparisons folder

---
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,56 @@
+document.write (typeof 5 )
+
+document.write (" 88" + 15 + " ");
+
+function my_Function() {
+    document.getElementById("Test ").innerHTML = 0/0;
+}
+
+function my_Function() {
+    document.getElementById("Test").innerHTML = isNaN('This is a string');
+}
+
+function my_Function2() {
+    document.getElementById("Test").innerHTML = isNaN('007');
+}
+
+document.write (2E415 + " ");
+
+document.write (-2E415 + " ");
+
+document.write (5>4 + " " );
+document.write (" ");
+document.write (5<4  + " "); 
+document.write (" ");
+document.write (15==15);
+document.write (" ");
+document.write (3==14);
+document.write (" ");
+X=15;
+Y=15;
+document.write (X===Y);
+document.write (" ");
+X=15;
+Y=20;
+document.write (X===Y);
+document.write (" ");
+X=15;
+Y="15";
+document.write (X===Y);
+document.write (" ");
+document.write (6>3 && 10>8);
+document.write (" ");
+document.write (6>4 && 10>11);
+document.write (" ");
+document.write (6>3 || 10>11);
+document.write (" ");
+document.write (6>7 || 10>11);
+document.write (" ");
+console.log(8+8);
+console.log(8<5)
+function not_Function() {
+    document.getElementById("Not").innerHTML= !(10>5)
+}
+function not_Function() {
+    document.getElementById("Not").innerHTML= !(5>10)
+}
